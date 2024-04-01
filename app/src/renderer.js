8036f88da9b18d1a09d7e9107fb0d820981273a1
Luke <rappalucas@gmail.com>
2024-04-01 15:16:43 -0400

    Fixed custom data loader, fixing electron app

--- app/src/renderer.js
+++ app/src/renderer.js
@@ -1,26 +1,29 @@
 // renderer.js
-const { ipcRenderer } = require('electron');
-
 const videoUpload = document.getElementById('videoUpload');
-const videoPlayer = document.getElementById('videoPlayer');
+const inputVideo = document.getElementById('inputVideo');
 const processBtn = document.getElementById('processBtn');
+const outputVideo = document.getElementById('outputVideo');
 const outputContainer = document.getElementById('outputContainer');
 
-videoUpload.addEventListener('change', function(event) {
-  const file = "output_video.mp4";
+videoUpload.addEventListener('change', function (event) {
+  const file = event.target.files[0];
   const url = URL.createObjectURL(file);
-  videoPlayer.src = url;
+  inputVideo.src = url;
 });
 
 processBtn.addEventListener('click', () => {
   const inputVideoPath = videoUpload.files[0].path;
-  ipcRenderer.send('process-video', inputVideoPath);
+  const outputVideoPath = window.electronAPI.join('output_video.mp4');
+  console.log('Sending process-video event with paths:', inputVideoPath, outputVideoPath);
+  window.electronAPI.send('process-video', inputVideoPath, outputVideoPath);
 });
 
-ipcRenderer.on('processing-update', (event, message) => {
+window.electronAPI.receive('processing-update', (message) => {
   outputContainer.innerHTML += `<p>${message}</p>`;
 });
 
-ipcRenderer.on('processing-complete', () => {
+window.electronAPI.receive('processing-complete', (outputVideoPath) => {
   outputContainer.innerHTML += '<p>Video processing completed.</p>';
+  outputVideo.src = outputVideoPath;
+  outputVideo.style.display = 'block';
 });
--- app/src/videoLoader.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('videoUpload').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    const url = URL.createObjectURL(file);
-    document.getElementById('videoPlayer').src = url;
-  });
-  
